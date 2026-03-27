a41569c30fedebb1b46c04d247f6c715ba8c2665
ilikebaconhi <tuanbrp@gmail.com>
2026-03-26 20:51:38 -0700

    Add files via upload

---
+++ Player.h
@@ -0,0 +1,34 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+#include "Hand.h"
+#include "Deck.h"
+
+using namespace std;
+
+class Player{
+    private:
+        string name;
+        Hand hand;
+        int chips;
+        int currrentBet;
+        int wins;
+
+    public: 
+        Player(string playerName = "Player", int startingChips = 100);
+        bool placeBet(int amount);
+        void winBet();
+        void loseBet();
+        void pushBet();
+
+        int getChips() const;
+        int getCurrentBet() const;
+        string getName() const;
+
+        void printHand();
+
+};
+
+
+#endif
