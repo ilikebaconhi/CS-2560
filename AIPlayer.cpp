5ea6ed8dd58f001e75a4fecbfaf328b9ff9fa83c
ilikebaconhi <tuanbrp@gmail.com>
2026-04-03 12:52:17 -0700

    Add files via upload

---
+++ AIPlayer.cpp
@@ -0,0 +1,50 @@
+#include "AIPlayer.h"
+
+AIPlayer::AIPlayer(string name, int startingChips)
+    : Player(name, startingChips){
+    Count = 0;    
+    }
+
+void AIPlayer::resetCount(){
+    Count = 0;
+}
+
+void AIPlayer::observeCard(const Card& card){
+    int val = card.getValue();
+
+    if(val >= 2 && val <= 6){
+        Count++;
+    }else if(val == 10 || val == 11){
+        Count--;
+    }
+}
+
+int AIPlayer::getCount() const{
+    return Count;
+}
+
+bool AIPlayer::shouldHit(){
+    int total = getHandValue();
+
+    if (Count >= 3) {
+        return total < 16;
+    }
+    else if (Count <= -3) {
+        return total < 18;
+    }
+    else {
+        return total < 17;
+    }
+}
+
+int AIPlayer::makeBet() const {
+    if (runningCount >= 3 && chips >= 50) {
+        return 50;
+    }
+    else if (chips >= 25) {
+        return 25;
+    }
+    else {
+        return chips;
+    }
+}
---
+++ AIPlayer.h
@@ -0,0 +1,21 @@
+#ifndef AIPLAYER__H
+#define AIPLAYER_H
+
+#include "Player.h"
+
+class AIPlayer : public Player{
+private:
+    int Count;
+
+public:
+    AIPlayer(string playerName = "Dealer", int startingChips = 1000);
+
+    void resetCount();
+    void observeCard(const Card& card);
+    int getCount() const;
+
+    bool shouldHit();
+    int makeBet() const;
+};
+
+#endif
