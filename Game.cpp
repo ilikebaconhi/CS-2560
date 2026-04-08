8d2b750658ac3c58093c3692bdc1b023dead8e07
ilikebaconhi <tuanbrp@gmail.com>
2026-04-07 20:50:11 -0700

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Deck.h"
+#include "Player.h"
+#include "AIPlayer.h"
+#include "Scoreboard.h"
+
+using namespace std;
+
+int main(){
+  string playAgain = "yes";
+
+  Player player("Player", 1000);
+}
